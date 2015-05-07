04566a2c6943487310982e9ea0679582eaafd08b
wikies.wan <wikies.wan@vipshop.com>
2015-05-07 19:43:07 +0800

    admin

--- config.js
+++ config.js
@@ -76,7 +76,7 @@ var config = {
   weibo_id: 'your_weibo_id',
 
   // admin 可删除话题，编辑标签，设某人为达人
-  admins: { admin: true },
+  admins: { admin: true,lyc152:true,linkarys:true },
 
   // github 登陆的配置
   GITHUB_OAUTH: {
